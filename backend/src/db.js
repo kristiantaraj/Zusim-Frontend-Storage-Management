7ba77e8721dc269aace6d36a8a6bb476a3954e11
Kristian Taraj <tarajkristian@gmail.com>
2026-05-15 14:05:52 +0200

    Set Prisma pool size to 2 to avoid max clients error

--- backend/src/db.js
+++ backend/src/db.js
@@ -7,8 +7,19 @@ try {
   ({ PrismaClient } = require('@prisma/client'));
 }
 
+
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+  // For Prisma >=4.15.0, pool config is supported
+  pool: {
+    max: 2,
+    min: 1,
+  },
 });
 
 module.exports = prisma;
